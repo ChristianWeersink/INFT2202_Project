a11a571d6a62410f70fa329e2e69fc21fe9152b3
cjackson100 <158119520+cjackson100@users.noreply.github.com>
2024-04-15 20:42:38 -0400

    Added photo carousel
    Added public folder, images folder, edited CSS, set moving carousel for images on homepage

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(cookieParser());
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use('/public/images', express.static('images'));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
